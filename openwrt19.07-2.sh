62cd23444fd8bb7bc0bb8a99ba58036381416672
zfzrx <151946331+zfzrx@users.noreply.github.com>
2026-09-17 17:58:23 +0800

    Update openwrt19.07-2.sh

--- openwrt19.07-2.sh
+++ openwrt19.07-2.sh
@@ -9,31 +9,18 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #=================================================
-# 修改 edge 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-# sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' openwrt/feeds/luci/collections/luci/Makefile
 
 # 单独拉取软件包
-#git clone -b default-openwrt-19.07 https://github.com/yuos-bit/other package/default-settings
 git clone -b default-19.07-Development https://github.com/yuos-bit/other package/default-settings
 git clone -b main-19.07 https://github.com/yuos-bit/other package/yuos
 
-# nft-fullcone
+# nft-fullcone（可选，建议先保留，编译失败再注释）
 git clone -b main --single-branch https://github.com/fullcone-nat-nftables/nftables-1.0.5-with-fullcone package/nftables
 git clone -b master --single-branch https://github.com/fullcone-nat-nftables/libnftnl-1.2.4-with-fullcone package/libnftnl
 
-# 最新主题
-# find feeds/luci -type d -name "luci-theme-bootstrap" -exec rm -rf {} +
-# rm -rf package/yuos/luci-theme-bootstrap
-# git clone -b master https://github.com/eamonxg/luci-theme-bootstrap package/yuos
-
-# cp -rf $GITHUB_WORKSPACE/patchs/4.14/themes/* packages/
-# cp -rf $GITHUB_WORKSPACE/patchs/4.14/themes/* feeds/packages/
-
-# 
 # 测试编译时间
 YUOS_DATE="$(date +%Y.%m.%d)(Development)"
 BUILD_STRING=${BUILD_STRING:-$YUOS_DATE}
-echo "Write build date in openwrt : $BUILD_DATE"
 echo -e '\nyuos Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
 sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
 echo "DISTRIB_REVISION=''" >> package/base-files/files/etc/openwrt_release
@@ -43,13 +30,15 @@ echo "DISTRIB_DESCRIPTION='yuos Build @ ${BUILD_STRING}'" >> package/base-files/
 # 修改 luci version.lua
 sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
 echo "luciversion = '${BUILD_STRING}'" >> feeds/luci/modules/luci-base/luasrc/version.lua
-# 
-#升级cmake
-find . -type d -name "cmake" -exec rm -r {} +
-mkdir -p tools/cmake/
-cp -rf $GITHUB_WORKSPACE/patchs/4.14/tools/cmake/* tools/cmake/
 
-#升级golang
-find . -type d -name "golang" -exec rm -r {} +
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+# ==================== 以下两段暂时注释，优先保证编译成功 ====================
+#升级cmake
+#find . -type d -name "cmake" -exec rm -r {} +
+#mkdir -p tools/cmake/
+#cp -rf $GITHUB_WORKSPACE/patchs/4.14/tools/cmake/* tools/cmake/
+
+#升级golang（19.07 不建议升级，容易失败）
+#find . -type d -name "golang" -exec rm -r {} +
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+# =======================================================================
