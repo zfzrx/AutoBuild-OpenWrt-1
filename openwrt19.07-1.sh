5de13624bb95713904c3ebc5f27d10ea3c25b1b4
zfzrx <151946331+zfzrx@users.noreply.github.com>
2026-09-17 17:34:07 +0800

    Update openwrt19.07-1.sh

--- openwrt19.07-1.sh
+++ openwrt19.07-1.sh
@@ -8,8 +8,8 @@ sed -i 's|https://github.com/Lienol/openwrt-luci.git;17.01|https://github.com/co
 # 增加软件包
 #sed -i 's#github.com/immortalwrt/packages.git;openwrt-21.02#github.com/yuos-bit/other.git;immortalwrt-packages-21.02#' feeds.conf.default
 #sed -i 's#github.com/immortalwrt/luci.git;openwrt-21.02#github.com/yuos-bit/other.git;immortalwrt-luci-21.02#' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
+#sed -i '$a src-git small8 https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
 
 
 # 覆盖源码
