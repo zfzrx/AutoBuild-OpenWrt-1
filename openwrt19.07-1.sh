ef912e95a48a288ebb081cda352c0b837afef519
zfzrx <151946331+zfzrx@users.noreply.github.com>
2026-09-18 11:11:29 +0800

    Update openwrt19.07-1.sh

--- openwrt19.07-1.sh
+++ openwrt19.07-1.sh
@@ -20,11 +20,11 @@ sed -i '/kenzok8/d' feeds.conf.default
 
 
 # 覆盖源码
-cp -rf $GITHUB_WORKSPACE/patchs/4.14/dts/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/dts/
-cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt76x8/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/
-cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt7621/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/
-cp -rf $GITHUB_WORKSPACE/patchs/4.14/board.d/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/base-files/etc/board.d/
-cp -rf $GITHUB_WORKSPACE/patchs/4.14/wifi/mac80211.sh $GITHUB_WORKSPACE/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#cp -rf $GITHUB_WORKSPACE/patchs/4.14/dts/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/dts/
+#cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt76x8/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/
+#cp -rf $GITHUB_WORKSPACE/patchs/4.14/mt7621/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/
+#cp -rf $GITHUB_WORKSPACE/patchs/4.14/board.d/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/base-files/etc/board.d/
+#cp -rf $GITHUB_WORKSPACE/patchs/4.14/wifi/mac80211.sh $GITHUB_WORKSPACE/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改默认dnsmasq为dnsmasq-full
 sed -i 's/dnsmasq-full/dnsmasq-full coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-ipt-raw6 kmod-tun luci-app-webadmin/g' include/target.mk
